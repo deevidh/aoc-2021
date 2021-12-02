b901a516645ef3859925ba767bf7a6768784ff68
David Halliwell <david.halliwell@cloudsoft.io>
2021-12-02 09:53:37 +0000

    Day 2 solution

---
+++ 2/2.py
@@ -0,0 +1,53 @@
+from aocd import data
+from aocd import submit
+from aocd import get
+
+# If set to True, use test data from the test_data.txt file
+USE_TEST_DATA=False
+
+def get_input():
+    if USE_TEST_DATA:
+        with open(f"{get.current_day()}/test_data.txt") as f:
+            data_lines = f.read().splitlines()
+    else:
+        data_lines = data.splitlines()
+    strings = [ line.split(" ") for line in data_lines ]
+    return [(direction, int(distance)) for [direction, distance] in strings]
+
+def part1(course):
+    distance = 0
+    depth = 0
+    for direction, amount in course:
+        if direction == "forward":
+            distance+=amount
+        if direction == "up":
+            depth-=amount
+        if direction == "down":
+            depth+=amount
+    return distance * depth
+
+def part2(course):
+    aim = 0
+    distance = 0
+    depth = 0
+    for direction, amount in course:
+        if direction == "forward":
+            distance+=amount
+            depth+=amount*aim
+        if direction == "up":
+            aim-=amount
+        if direction == "down":
+            aim+=amount
+    return distance * depth
+
+input_data = get_input()
+
+## Part 1
+my_answer=part1(input_data)
+print(f"Part 1: {my_answer}")
+#submit(my_answer)
+
+## Part 2
+my_answer=part2(input_data)
+print(f"Part 2: {my_answer}")
+#submit(my_answer)
---
+++ 2/README.md
@@ -0,0 +1,7 @@
+# AoC - Day 2
+
+[https://adventofcode.com/2021/day/2](https://adventofcode.com/2021/day/2)
+
+Not much to say about this challenge, it's fairly straightforward. It's nice to use tuple unpacking in the for loops, to avoid having to access the data by index.  It also allows using a descriptive variable.
+
+I have used descriptive variable names throughout rather than x, y etc. Of course this is a little slower to type, and can make the code look a bit chunky.  However it can also help to avoid mistakes with muddling up variables, or forgetting which directions your co-ordinates go.
